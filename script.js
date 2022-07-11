1295f4eecdf8d11d676ee29569c1969f0f1184ca
Yorbit <marco.95112@hotmail.it>
2022-07-11 17:17:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const root = new Vue(
+    {
+
+        name: "First Vue",
+        el: "#present-vue",
+        data: {
+            firstName: "Marco",
+            lastName: "Crippa",
+            avatar: "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456341.png"
+        }
+    }
+);
